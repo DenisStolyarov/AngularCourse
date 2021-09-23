e2c0b5ec3eed7df0b79bb85336bff98a0a904111
Dzianis Staliarou <Dzianis_Staliarou@epam.com>
2021-09-23 18:33:35 +0300

    Add Product service and model.

---
+++ src/app/products/models/product.model.ts
@@ -0,0 +1,11 @@
+/**
+ * Product Model
+ */
+export class Product {
+    constructor(
+        id: number,
+        name: string,
+        description: string,
+        price: number,
+    ) { }
+}
---
+++ src/app/products/services/products.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor() { }
+
+  getProducts(): Array<Product> {
+    return [
+      new Product(1, 'Telephone', 'Useful gadget for communication', 100),
+      new Product(2, 'Microwave', 'Useful gadget for food', 200),
+      new Product(3, 'Laptop', 'Useful gadget for work', 700),
+    ];
+  }
+}
