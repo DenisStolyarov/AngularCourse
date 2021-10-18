93b04e75f4e578ad6e0261e821e89df1a0ce26c1
Dzianis Staliarou <Dzianis_Staliarou@epam.com>
2021-10-18 21:04:18 +0300

    Add title to the app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularCourse';
+export class AppComponent implements AfterViewChecked {
+  @ViewChild('appTitle')
+  titleField!: ElementRef<HTMLHeadingElement>;
+
+  ngAfterViewChecked(): void {
+    this.titleField.nativeElement.innerText = 'AngularCourse';
+  }
 }
